f46ed3a7bf60997ec447acc1f07fc2bfee2dfff8
vk59 <jkostylev592@gmail.com>
2023-04-21 16:16:39 +0300

    Initial commit.

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/user/create', methods=['POST'])
+def create_user():
+    return
